73d7b3b4d54ec11a185fe5caa11ee121473a569d
Vishal Malhotra <vishal.malhotra@lapr245.successivetech.com>
2020-01-27 17:59:05 +0530

    refactor: Update diamondPattern.js and equilateralTriangle.js

---
+++ extra/patterns/diamondPattern.js
@@ -0,0 +1,75 @@
+
+// Function  for implementing diamond triangle
+const diamond = (row) => {
+  let row_Index = 0;
+  let space = "";
+  let star = "";
+  let space_Count = 0;
+
+
+  // Logic for upper triangle 
+
+  for (row_Index = 0; row_Index < row; row_Index++) {
+
+    for (let k = row - 1; k > row_Index; k--) {
+      space = space + " ";
+    }
+
+    for (let j = 0; j < row_Index * 2 + 1; j++) {
+      star = star + "*";
+    }
+    console.log(space + star);
+    star = "";
+    space = "";
+
+  }
+
+  // Logic for lower triangle
+  for (let i = row_Index; i < row * 2; i++) {
+
+    for (let j = 0; j < space_Count; j++) {
+      space = space + " ";
+    }
+
+    for (let k = 0; k < (row - space_Count) * 2 - 1; k++) {
+      star = star + "*";
+
+    }
+
+    space_Count++;
+    console.log(space + star);
+    space = "";
+    star = "";
+  }
+}
+
+
+
+
+let rows = process.argv[2];
+
+// Creating regex for input constraints
+const regex = /^([2-9]|1[0])$/;
+
+//Using try catch for throwing errors
+try {
+
+  // Regex for checking valid inputs
+
+
+  if (regex.test(rows)) {
+
+    diamond(rows);
+
+  }
+  else {
+
+    throw Error("Invalid Input");
+
+
+  }
+}
+catch (Error) {
+  console.log(Error.message);
+
+}
