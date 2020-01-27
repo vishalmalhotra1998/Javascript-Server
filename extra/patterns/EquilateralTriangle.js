20254cd31f8e5635488dcfb9cf4cad19d203c565
Vishal Malhotra <vishal.malhotra@lapr245.successivetech.com>
2020-01-27 12:38:59 +0530

    refactor: update Diamondpattern.js and EquilateralTriangle.js

--- extra/patterns/EquilateralTriangle.js
+++ extra/patterns/EquilateralTriangle.js
@@ -1,33 +1,27 @@
 
-function Equilateral(row)
-{
+function Equilateral(row) {
 
-for(let i=0;i<row;i++)
-{
+    for (let i = 0; i < row; i++) {
 
-    for(let k=0;k<row-i;k++)
-    {
-        process.stdout.write(" ");
+        for (let k = 0; k < row - i; k++) {
+            process.stdout.write(" ");
+
+        }
+        for (let j = 0; j <= i; j++) {
+
+            process.stdout.write("* ");
+        }
+
+
+        process.stdout.write("\n");
 
     }
-for(let j=0;j<=i;j++)
-{
-
-process.stdout.write("* ");
-} 
-
-
-process.stdout.write("\n");
-
-}
 }
 
-let num=process.argv[2];
-if(num>=2 && num<=10)
-{
+let num = process.argv[2];
+if (num >= 2 && num <= 10) {
     Equilateral(num);
 }
-else
-{
-process.stdout.write("range b/w 2 to 10");
+else {
+    process.stdout.write("range b/w 2 to 10");
 }
