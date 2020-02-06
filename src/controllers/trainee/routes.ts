49614d7d3e249084f3b93cf483730073c30aca0a
Vishal Malhotra <vishal.malhotra@lapr245.successivetech.com>
2020-02-06 17:14:07 +0530

    feat: add authMiddleWare.ts and permission.ts

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import traineeController from './Controller';
 import validationChecker from '../../libs/routes/validationHandler';
 import validation from './validation';
+import authMiddleware from '../../libs/routes/authMiddleWare';
 
 const routeHandler = Router();
 
-routeHandler.get('/', validationChecker(validation.get), traineeController.get);
+routeHandler.get('/', authMiddleware('getUsers', 'read'), validationChecker(validation.get), traineeController.get);
 routeHandler.post('/', validationChecker(validation.create), traineeController.post);
 routeHandler.put('/', validationChecker(validation.update), traineeController.put);
 routeHandler.delete('/:id', validationChecker(validation.delete), traineeController.delete);
