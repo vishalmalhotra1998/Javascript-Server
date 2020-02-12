9899d94b2d235238493e8132771f1c187e49a24d
Vishal Malhotra <vishal.malhotra@lapr245.successivetech.com>
2020-02-12 14:51:22 +0530

    refactor: Update validationHandler.ts

--- src/libs/routes/validationHandler.ts
+++ src/libs/routes/validationHandler.ts
@@ -1,56 +1,64 @@
 export default (config) => {
   return (req, res, next) => {
     const arr: string[] = [];
+    // Function for checking Key Validation
+    const checkForOtherValues = (configKey, reqLocationKey, key, reqKeyValue): void => {
+      if (configKey.string && typeof (reqLocationKey) !== 'string') {
+        arr.push(`${key} should be String`);
+      }
+      if (configKey.number) {
+        if (isNaN(reqLocationKey)) {
+          arr.push(`${configKey.errorMessage}`);
+        }
+      }
+      // If key contains regex module
+      if (configKey.regex) {
+        const regexString = new RegExp(configKey.regex);
+        if (reqKeyValue.trim().length) {
+          if (!regexString.test(reqKeyValue)) {
+            arr.push(`Invalid ${key}`);
+          }
+        }
+        else {
+          arr.push(`${configKey.errorMessage}`);
+        }
+      }
+      // If Key contains isObject and Checks the Object is retrived from Location
+      if (configKey.isObject && !(typeof reqLocationKey === 'object')) {
+        arr.push('Data is required of type object');
+      }
+      else {
+        if (Array.isArray(reqLocationKey)) {
+          arr.push('Data is required of type object');
+        }
+      }
+      // If key Contains Custom function
+      if (configKey.custom) {
+        try {
+          configKey.custom(reqLocationKey);
+        }
+        catch (err) {
+          arr.push(err.error);
+        }
+      }
+    };
     Object.keys(config).forEach(key => {
       // Check the In key for validations
       if (config[key].in) {
         config[key].in.forEach(location => {
-          let keyValue = req[location][key];
-          const values = Object.keys(req[location]);
+          const reqKeyValue = req[location][key];
+          if (config[key].required) {
+            // if key is required
+            checkForOtherValues(config[key], req[location][key], key, reqKeyValue);
+          }
+          else {
+            // if key is not required
+            if (req[location].hasOwnProperty(key)) {
+              // if property exist
+              checkForOtherValues(config[key], req[location][key], key, reqKeyValue);
 
-          // If Body has this key is Presented
-          if (config[key].required && !req[location][key]) {
-            arr.push(`${key} is required`);
-          }
-          // Check for the string
-          if (config[key].string && typeof (req[location][key]) !== 'string') {
-            arr.push(`${key} should be String`);
-          }
-          // Check For Skip and limit
-          if (config[key].number && typeof (req[location][key] !== 'number')) {
-            if (values.includes(key)) {
-              if (!keyValue && config[key].default) {
-                keyValue = config[key].default;
-              }
-              if (isNaN(req[location][key])) {
-                arr.push(`${key} should be number`);
-              }
-            }
-            else {
-              arr.push(`${key} should be Valid`);
             }
           }
-          // If key contains regex module
-          if (config[key].regex) {
-            const regexString = new RegExp(config[key].regex);
-            if (!regexString.test(keyValue)) {
-              arr.push(`Invalid ${key}`);
-            }
-          }
-          // If Key contains isObject and Checks the Object is retrived from Body
-          if (config[key].isObject && !(typeof req[location][key] === 'object')) {
-            arr.push('Data is required of type object');
-          }
-          // If key Contains Custom function
-          if (config[key].custom) {
-            try {
-              config[key].custom(req[location][key]);
-            }
-            catch (err) {
-              arr.push(err.error);
-            }
-          }
-
         });
       }
     });
