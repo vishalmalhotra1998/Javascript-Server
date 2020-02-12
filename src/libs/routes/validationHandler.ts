4d83bb5d9e9a9a94da4bfad7b9a15271513f7c2f
Vishal Malhotra <vishal.malhotra@lapr245.successivetech.com>
2020-02-12 17:35:40 +0530

    refactor: validationHandler.ts

--- src/libs/routes/validationHandler.ts
+++ src/libs/routes/validationHandler.ts
@@ -2,34 +2,33 @@ export default (config) => {
   return (req, res, next) => {
     const arr: string[] = [];
     // Function for checking Key Validation
-    const checkForOtherValues = (configKey, reqLocationKey, key, reqKeyValue): void => {
+    const checkForOtherValues = (configKey, reqLocationKey, key): void => {
       if (configKey.string && typeof (reqLocationKey) !== 'string') {
         arr.push(`${key} should be String`);
       }
-      if (configKey.number) {
+      else if (configKey.number) {
         if (isNaN(reqLocationKey)) {
           arr.push(`${configKey.errorMessage}`);
         }
       }
+      else if (configKey.isObject && typeof reqLocationKey !== 'object') {// If Key contains isObject and Checks the Object is retrived from Location
+        arr.push(`${key} is required of type object`);
+      }
+      else if (configKey.isObject && Array.isArray(reqLocationKey)) {
+        arr.push(`${key} should not be in type Array`);
+      }
       // If key contains regex module
-      if (configKey.regex) {
+      if (configKey.regex && configKey.regex.length) {
         const regexString = new RegExp(configKey.regex);
-        if (reqKeyValue.trim().length) {
-          if (!regexString.test(reqKeyValue)) {
+        reqLocationKey = typeof reqLocationKey === 'string' ? reqLocationKey.trim() : reqLocationKey;
+
+        if (typeof reqLocationKey !== 'string' || (typeof reqLocationKey === 'string' && reqLocationKey.length)) {
+          if (!regexString.test(reqLocationKey)) {
             arr.push(`Invalid ${key}`);
           }
         }
         else {
           arr.push(`${configKey.errorMessage}`);
-        }
-      }
-      // If Key contains isObject and Checks the Object is retrived from Location
-      if (configKey.isObject && !(typeof reqLocationKey === 'object')) {
-        arr.push('Data is required of type object');
-      }
-      else {
-        if (Array.isArray(reqLocationKey)) {
-          arr.push('Data is required of type object');
         }
       }
       // If key Contains Custom function
@@ -47,15 +46,12 @@ export default (config) => {
       if (config[key].in) {
         config[key].in.forEach(location => {
           const reqKeyValue = req[location][key];
-          if (config[key].required) {
-            // if key is required
-            checkForOtherValues(config[key], req[location][key], key, reqKeyValue);
+          if (config[key].required) {// if key is required
+            checkForOtherValues(config[key], req[location][key], key);
           }
-          else {
-            // if key is not required
-            if (req[location].hasOwnProperty(key)) {
-              // if property exist
-              checkForOtherValues(config[key], req[location][key], key, reqKeyValue);
+          else {// if key is not required
+            if (req[location].hasOwnProperty(key)) {// if property exist
+              checkForOtherValues(config[key], req[location][key], key);
 
             }
           }
