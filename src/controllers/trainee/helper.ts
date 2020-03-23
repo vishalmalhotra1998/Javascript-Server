fec68cc89fdc20c5ce016b3530aeeb8f95b2e5ec
Vishal Malhotra <vishal.malhotra@lapr245.successivetech.com>
2020-03-23 09:23:52 +0530

    fix: update helper.ts

--- src/controllers/trainee/helper.ts
+++ src/controllers/trainee/helper.ts
@@ -1,8 +1,8 @@
-export const searching = async (keysOfSearchObject, searchObject, options): Promise<{}> => {
+export const searching = (keysOfSearchObject, searchObject, options) => {
   const searchData = {};
   keysOfSearchObject.map((key) => {
     searchData[key] = { $regex: `^${searchObject[key]}`, $options: 'i' };
   });
 
-  return await searchData;
+  return searchData;
 };
