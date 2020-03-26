33751bdc39813301dfe881989e6a0200b64e38eb
Vishal Malhotra <vishal.malhotra@lapr245.successivetech.com>
2020-03-26 20:15:26 +0530

    fix: fix all issues

--- src/libs/routes/authMiddleWare.ts
+++ src/libs/routes/authMiddleWare.ts
@@ -12,7 +12,7 @@ export default  (module, permissionType) => async (req: IRequest, res: Response,
   const token: string = req.headers.authorization;
   const { SECRET_KEY: secretKey } = config;
   const decodeUser = jwt.verify(token, secretKey);
-  const userRepository = new UserRepository();
+  const userRepository: UserRepository = new UserRepository();
   if (!decodeUser) {
     next({
       status: 403,
--- src/repositories/user/UserRepository.ts
+++ src/repositories/user/UserRepository.ts
@@ -5,14 +5,13 @@ import VersionRepository from '../versionable/VersionRepository';
 
 class UserRepository extends VersionRepository<IUserModel, mongoose.Model<IUserModel>> {
 
-  private userModel: mongoose.Model<IUserModel>;
   constructor() {
     super(userModel);
   }
   static generateObjectId() {
     return String(mongoose.Types.ObjectId());
   }
-  create = (data: any, authId= {}): Promise<IUserModel> => {
+  create = (data: any, authId= {}) => {
     return super.create(data, authId);
   }
 
@@ -20,19 +19,19 @@ class UserRepository extends VersionRepository<IUserModel, mongoose.Model<IUserM
     return super.count();
   }
 
-  list = (query: any = {}, options: any = {}): Promise<IUserModel[]> => {
+  list =  (query: any = {}, options: any = {}) => {
     return super.list(query, options);
   }
 
-  delete = (data: any): Promise<IUserModel> => {
+  delete =  (data: any) => {
     return super.delete(data);
   }
 
-  update = (data: object, dataToUpdate: object): Promise<IUserModel> => {
+  update =  (data: object, dataToUpdate: object) => {
     return super.update(data, dataToUpdate);
   }
 
-  get = (query: object): Promise<IUserModel> => {
+  get =  (query: object) => {
     return super.get(query);
   }
 }
